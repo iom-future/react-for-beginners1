d4747fa4e35e54ec9bb536bf6e5f14c34007e941
iom_future <isaacmoses11111@gmail.com>
2025-12-06 06:57:34 +0100

    learn props and how to pass props from parent to children

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@
 import UserList from "./components/UserList.jsx"
 import ProductList from "./components/ProductList.jsx";
 import Renderchallenge from "./components/RenderChallenge.jsx"
+import Person from "./components/Person.jsx";
+import Product from "./components/Product.jsx";
+import ChildrenProps from "./components/ChildrenProps.jsx";
 function App() {
     return(<div>
-     <Renderchallenge />
-     
+     <Person name="IOM" age={17} />
+     <Product name="laptop" price={90000} />
+     <ChildrenProps>
+        <h1>children prop heading</h1>
+        <p>children props paragraph</p>
+     </ChildrenProps>
     </div>);
 }
 
