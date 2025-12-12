142d0d9da61dad5f059927aa99bf44927a2bf16b
iom_future <isaacmoses11111@gmail.com>
2025-12-11 19:40:53 -1100

    learn about ContextAPI: passing multiple prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import { createContext } from "react"; //import the context API
 import ComponentA from "./components/ComponentA.jsx";
 
 export const ContextAPI = createContext() //create an instance of the context object
+export const ContextAPI1 = createContext() //create a second instance of the context object
 function App() {
-    let dataToDeeplyNestedComponent = "data for component C"
+    let dataToDeeplyNestedComponent = "prop for component C";
+    let  dataToDeeplyNestedComponent1 = "second prop for component C"
     return(<div>
-
-        {/* wrap what needs the prop in a  Data.Provider Component; within it, is what should receive the prop */}
         <ContextAPI.Provider value={dataToDeeplyNestedComponent}>
-            <ComponentA />
+            <ContextAPI1.Provider value={dataToDeeplyNestedComponent1} >
+                 <ComponentA />
+            </ContextAPI1.Provider>
+           
         </ContextAPI.Provider>
        {/* we are drilling this data into componentC  */}
 
--- src/components/ComponentC.jsx
+++ src/components/ComponentC.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 //import the contextAPI where ever you need the prop
-import { ContextAPI } from '../App'
+import { ContextAPI, ContextAPI1 } from '../App'
 function ComponentC() {
   return (
     <div>
         <ContextAPI.Consumer>
-                {/*the consumer components takes a call back function (while the provider takes the component you want to pass data into) */}
-                {/* the call back function returns the element that use he prop that was provided */}
-                {(props)=>{
-                    return(<h1>{props}</h1> )
+           {/* each ContextAPI.Consumer component must start/have with a call back function, and the call back function must return something/what consumes/uses the prop data */}
+                {(propFromContextAPI)=>{
+                    return(
+                        <ContextAPI1>
+                            {(propFromContextAPI1)=>{
+                                    return <h2>this is prop data 1: {propFromContextAPI}, this is prop data 2: {propFromContextAPI1}</h2>
+                            }}
+                        </ContextAPI1>
+                    )
                 }}
+           
+                
         </ContextAPI.Consumer>
    </div>
   )
